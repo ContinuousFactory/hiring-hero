5a574448bbe53c6f2194b0ef38ccf7664062132d
Long Nguyen <long.polyglot@gmail.com>
2016-10-09 14:21:54 +0700

    Refactor registration template to use haml

--- web/plugs/subdomain.ex
+++ web/plugs/subdomain.ex
@@ -10,10 +10,16 @@ defmodule Hiringhero.Subdomain do
     user = Hiringhero.Repo.preload(user, [:my_organisation, :organisation])
     if user.my_organisation do
       org = user.my_organisation
-    else
+    end
+
+    if user.organisation do
       org = user.organisation
     end
-    assign(conn, :subdomain, org.subdomain)
-    assign(conn, :current_organisation, org)
+
+    if org do
+      assign(conn, :subdomain, org.subdomain)
+      assign(conn, :current_organisation, org)
+    end
+    conn
   end
 end
