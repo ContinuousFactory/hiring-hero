b84c84eaf00d65a0e0c2dff005cabb679ad5c6ff
Long Nguyen <long.polyglot@gmail.com>
2016-11-15 23:21:48 +0700

    Fix minor bug

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -22,7 +22,7 @@ defmodule Hiringhero.UserController do
       {:ok, user} ->
         conn
         |> put_flash(:info, "User updated successfully.")
-        |> redirect(to: user_path(conn, :show, user))
+        |> redirect(to: user_path(conn, :edit, user))
       {:error, changeset} ->
         render(conn, "edit.html", user: user, changeset: changeset)
     end
