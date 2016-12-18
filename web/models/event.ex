097db310c254f8690066fcdc8b078c6edc3fcac9
Long Nguyen <long.polyglot@gmail.com>
2016-12-18 22:50:58 +0700

    Implement string to date func

--- web/models/event.ex
+++ web/models/event.ex
@@ -14,9 +14,19 @@ defmodule Hiringhero.Event do
   end
 
   def changeset(model, params \\ %{}) do
+    params = date_string_to_ecto_date(params)
     model
-    |> cast(params, [:title, :description, :happen_at, :priority])
-    |> cast_assoc(:organisation)
+    |> cast(params, [:title, :description, :happen_at, :priority, :organisation_id])
     |> validate_required([:title, :happen_at, :organisation_id])
+  end
+
+  def date_string_to_ecto_date(params) do
+    if Map.has_key?(params, "happen_at") do
+      {:ok, time} = params["happen_at"] |> Timex.parse("{0M}/{0D}/{YYYY} {h12}:{m} {am}")
+      ecto_time = time |> Ecto.DateTime.cast!
+      %{params | "happen_at" => ecto_time }
+    else
+      params
+    end
   end
 end
